2c0bb29a7ba9e70b474eedee87e2422f2fd4cf4e
mok17 <mok17@up.edu>
2015-09-03 16:39:39 -0700

    Fixed Code for PrimeTester

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -14,7 +14,17 @@ public class PrimeTester {
      * @return true iff n is prime
      */
     public static boolean isPrime(long n) {
-        // for now, return a random result
-        return Math.random() > 0.5;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
